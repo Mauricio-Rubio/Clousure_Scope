cb6169de116de7e9700b994c3974a98f15fa7ca3
Mauricio-Rubio <mau.r.h54@gmail.com>
2022-07-15 01:08:06 -0500

    Add presentation

--- src/closure/example.js
+++ src/closure/example.js
@@ -11,10 +11,10 @@ moneyBox(5);
 */
 
 function moneyBox() {
-  let saveCoins = 0;
+  let _saveCoins = 0;
 
   return function savaMoney(coins) {
-    return (saveCoins += coins);
+    return (_saveCoins += coins);
   };
 }
 
@@ -28,3 +28,30 @@ console.log("-----------");
 console.log(DiegoBox(20));
 console.log(DiegoBox(10));
 console.log(DiegoBox(10));
+
+// Ejemplo de closures asemejando una clase
+const contador = (function(){
+  let _contador = 0;
+  function incrementar() {
+      return _contador++;
+  }
+  function decrementar() {
+      return _contador--;
+  }
+
+  function getValor() {
+    return _contador;
+  }
+
+  return {
+    incrementar, decrementar, getValor
+  }
+})();
+
+console.log(contador.getValor());
+contador.incrementar();
+contador.incrementar();
+contador.incrementar();
+contador.incrementar();
+contador.decrementar();
+console.log(contador.getValor());
